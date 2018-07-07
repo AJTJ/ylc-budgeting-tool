d27c7c6fb846dea37f6d36ba10c30e2c354154d8
Aaron Janke <aaron@windscribe.com>
2018-07-07 13:07:35 -0400

    initializing project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>YLC Budgeting tool!!!</Row>
+      </Container>
+    );
+  }
+}
+
+export default App;
