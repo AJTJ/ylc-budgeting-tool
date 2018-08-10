eada14cb359503ad37f33bb53fe774e0dedd8cbb
Aaron Janke <janke.aaron@gmail.com>
2018-08-10 13:49:42 -0400

    building tools

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
+Project Stack
+
+Structure
+- React
+- Redux
+
+Styling
+- emotion
+- reactstrap
+
+Forms
+- formik
+
+Data visualization
+- nivo
+
+PDF creation
+- react-pdf
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row } from "reactstrap";
+import { Pie } from "@nivo/pie";
+
+let pieData = [
+  {
+    id: "dogs",
+    value: 45
+  },
+  {
+    id: "cats",
+    value: 55
+  }
+];
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Row>YLC Budgeting tool!!!</Row>
-      </Container>
+      <div>
+        <Container>
+          <Row>YLC Budgeting tool!!!</Row>
+          <Row>
+            <div>
+              <Pie data={pieData} height={300} width={300} />
+            </div>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
