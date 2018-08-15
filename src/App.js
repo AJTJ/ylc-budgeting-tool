009c1e3dcfa9aab668f3dd324e0ba5f79b2145de
Aaron Janke <janke.aaron@gmail.com>
2018-08-15 13:57:39 -0400

    building liveUpdate functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
 
 import Pie from "./views/dashboard/Pie";
@@ -8,11 +8,26 @@ import { Form } from "./views/Form";
 
 import { Container } from "reactstrap";
 
-export default () => (
-  <Container fluid>
-    <Trail />
-    <MiniBudget />
-    <Pie />
-    <Route path="/Form/:formType" component={Form} />
-  </Container>
-);
+class App extends Component {
+  state = {
+    income: 345,
+    expenses: 555
+  };
+
+  handleChange = (id, amount) => {
+    return this.setState({ [id]: [amount] });
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Trail />
+        <MiniBudget {...this.state} />
+        <Pie {...this.state} />
+        <Route path="/Form/:formType" handleChange component={Form} />
+      </Container>
+    );
+  }
+}
+
+export default App;
--- src/views/dashboard/MiniBudget.js
+++ src/views/dashboard/MiniBudget.js
@@ -2,18 +2,21 @@ import React from "react";
 
 import { Container, Row, Col } from "reactstrap";
 
-export default props => (
-  <Container>
-    <Row>Current Budget</Row>
+export default props => {
+  console.log(props);
+  return (
+    <Container>
+      <Row>Current Budget</Row>
 
-    <Row>
-      <Col>Income</Col>
-      <Col>25235</Col>
-    </Row>
+      <Row>
+        <Col>Income</Col>
+        <Col>25235</Col>
+      </Row>
 
-    <Row>
-      <Col>Expenses</Col>
-      <Col>{props.expenses}</Col>
-    </Row>
-  </Container>
-);
+      <Row>
+        <Col>Expenses</Col>
+        <Col>{props.expenses}</Col>
+      </Row>
+    </Container>
+  );
+};
