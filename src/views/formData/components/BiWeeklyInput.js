38c525f8258fc2341b0a0e138073e9e3d87819ee
Aaron Janke <janke.aaron@gmail.com>
2018-09-13 18:44:45 -0500

    slowly yet suretly

--- src/views/formData/components/BiWeeklyInput.js
+++ src/views/formData/components/BiWeeklyInput.js
@@ -1,4 +1,7 @@
 import React from "react";
+
+import Component from "@reactions/component";
+
 import {
   FormGroup as RSFormGroup,
   Label as RSLabel,
@@ -48,26 +51,42 @@ const BiWeeklyDisplay = money => {
   return <Row>{money}</Row>;
 };
 
-export default ({ Component = Input, ...props }) => (
-  <React.Fragment>
-    <FormGroup row>
-      <Col className="d-flex align-items-center" xs={12} sm={12} md={12} lg={5}>
-        {props.text ? (
-          <React.Fragment>
-            <Col xs={10} sm={10} md={10} lg={10}>
+export default ({ InputComponent = Input, ...props }) => (
+  <Component initialState={{ biWeekly: 0 }}>
+    {({ setState, state }) => (
+      <React.Fragment>
+        <FormGroup row>
+          <Col
+            className="d-flex align-items-center"
+            xs={12}
+            sm={12}
+            md={12}
+            lg={5}
+          >
+            {props.text ? (
+              <React.Fragment>
+                <Col xs={10} sm={10} md={10} lg={10}>
+                  <Label className="m-0">{props.name}</Label>
+                </Col>
+                <Question src={help} alt="Question Mark Icon" id={props.id} />
+                <Tooltip target={props.id} text={props.text} />
+              </React.Fragment>
+            ) : (
               <Label className="m-0">{props.name}</Label>
-            </Col>
-            <Question src={help} alt="Question Mark Icon" id={props.id} />
-            <Tooltip target={props.id} text={props.text} />
-          </React.Fragment>
-        ) : (
-          <Label className="m-0">{props.name}</Label>
-        )}
-      </Col>
-      <Col xs={12} sm={12} md={12} lg={7}>
-        <Component {...props} />
-        {/* <Row>{BiWeeklyDisplay(55)}</Row> */}
-      </Col>
-    </FormGroup>
-  </React.Fragment>
+            )}
+          </Col>
+          <Col xs={12} sm={12} md={12} lg={7}>
+            <InputComponent
+              {...props}
+              onChange={e => {
+                console.log(e);
+                setState(state => ({ biWeekly: state.biWeekly + 1 }));
+              }}
+            />
+            {state.biWeekly}
+          </Col>
+        </FormGroup>
+      </React.Fragment>
+    )}
+  </Component>
 );
